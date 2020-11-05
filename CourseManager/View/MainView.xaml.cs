e77601b8cb7d5f0d6c1e46208dfeaad20504604a
Sheldon <sheldon_li@asus.com>
2020-11-05 09:47:28 +0800

    初始化基本框架,登录界面搭建

--- CourseManager/View/MainView.xaml.cs
+++ CourseManager/View/MainView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CourseManager
+namespace Course.View
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
