0a960e84a9c8f65ca0af93c9cdb89171af2b1578
SheldonSir <sheldon_sir@163.com>
2020-11-25 09:38:02 +0800

    09-登录界面-关闭窗口-命令绑定

--- CourseManager/View/LoginView.xaml.cs
+++ CourseManager/View/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Course.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace Course.View
         public LoginView()
         {
             InitializeComponent();
+
+            this.DataContext = new LoginViewModel();
         }
 
     }
---
+++ CourseManager/ViewModel/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Course.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Course.ViewModel
+{
+    class LoginViewModel
+    {
+        public CommandBase CloseWindowCmd { get; set; }
+
+        public LoginViewModel()
+        {
+            this.CloseWindowCmd = new CommandBase();
+            this.CloseWindowCmd.DoExecute = new Action<object>((o) =>
+            {
+                (o as Window).Close();
+            });
+
+            this.CloseWindowCmd.DoCanExecute = new Func<object, bool>((o)=> { return true; });
+        }
+    }
+}
